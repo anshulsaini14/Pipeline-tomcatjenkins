a7c108c77a727da7d19a4ef54ab076bc55e5258a
anshulsaini14 <148077920+anshulsaini14@users.noreply.github.com>
2024-11-26 17:40:14 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return " This is updated Jenkins Pipeline Project.";
+        return " This is updated Jenkins Pipeline Project for automatic update.";
     }
 
 }
